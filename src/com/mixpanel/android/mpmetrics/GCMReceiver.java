d7de314d7e5e535885f8083eddbce7f5d6413265
Alec Lee <alec.lee15@gmail.com>
2012-07-24 14:41:26 -0700

    eclipse whitespacing

--- src/com/mixpanel/android/mpmetrics/GCMReceiver.java
+++ src/com/mixpanel/android/mpmetrics/GCMReceiver.java
@@ -37,33 +37,33 @@ public class GCMReceiver extends BroadcastReceiver {
                 mp.removePushRegistrationId();             
             }
         } else if ("com.google.android.c2dm.intent.RECEIVE".equals(action)) {
-                String message = intent.getExtras().getString("mp_message");
+            String message = intent.getExtras().getString("mp_message");
 
-                if (message == null) return;
-                if (MPConfig.DEBUG) Log.d(LOGTAG, "MP GCM notification received: " + message);
+            if (message == null) return;
+            if (MPConfig.DEBUG) Log.d(LOGTAG, "MP GCM notification received: " + message);
 
-                PackageManager manager = context.getPackageManager();
-                Intent appIntent = manager.getLaunchIntentForPackage(context.getPackageName());
-                CharSequence notificationTitle = "";
-                int notificationIcon = android.R.drawable.sym_def_app_icon;
-                try {
-                    ApplicationInfo appInfo = manager.getApplicationInfo(context.getPackageName(), 0);
-                    notificationTitle = manager.getApplicationLabel(appInfo);
-                    notificationIcon = appInfo.icon;
-                } catch (NameNotFoundException e) { }
+            PackageManager manager = context.getPackageManager();
+            Intent appIntent = manager.getLaunchIntentForPackage(context.getPackageName());
+            CharSequence notificationTitle = "";
+            int notificationIcon = android.R.drawable.sym_def_app_icon;
+            try {
+                ApplicationInfo appInfo = manager.getApplicationInfo(context.getPackageName(), 0);
+                notificationTitle = manager.getApplicationLabel(appInfo);
+                notificationIcon = appInfo.icon;
+            } catch (NameNotFoundException e) { }
 
-                PendingIntent contentIntent = PendingIntent.getActivity(
-                    context.getApplicationContext(),
-                    0,
-                    appIntent,   // add this pass null to intent
-                    PendingIntent.FLAG_UPDATE_CURRENT
-                );
+            PendingIntent contentIntent = PendingIntent.getActivity(
+                context.getApplicationContext(),
+                0,
+                appIntent,   // add this pass null to intent
+                PendingIntent.FLAG_UPDATE_CURRENT
+            );
 
-                NotificationManager nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-                Notification n = new Notification(notificationIcon, message, System.currentTimeMillis());
-                n.flags |= Notification.FLAG_AUTO_CANCEL;
-                n.setLatestEventInfo(context, notificationTitle, message, contentIntent);
-                nm.notify(0, n);
+            NotificationManager nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+            Notification n = new Notification(notificationIcon, message, System.currentTimeMillis());
+            n.flags |= Notification.FLAG_AUTO_CANCEL;
+            n.setLatestEventInfo(context, notificationTitle, message, contentIntent);
+            nm.notify(0, n);
         }
     }
 }
