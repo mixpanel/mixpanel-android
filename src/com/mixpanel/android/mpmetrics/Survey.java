c6df26204d2b1efddefedc32baebb384f32b3cea
Joe Bowers <joe@mixpanel.com>
2013-10-11 17:50:41 -0700

    Missing assets

---
+++ src/com/mixpanel/android/mpmetrics/Survey.java
@@ -0,0 +1,8 @@
+package com.mixpanel.android.mpmetrics;
+
+public class Survey {
+
+    /* package */ Survey() {
+    }
+
+}
---
+++ src/com/mixpanel/android/mpmetrics/SurveyCallbacks.java
@@ -0,0 +1,15 @@
+package com.mixpanel.android.mpmetrics;
+
+/**
+ * For use with MixpanelAPI.checkForSurvey
+ */
+public interface SurveyCallbacks {
+    /**
+     * foundSurvey will be called when the MixpanelAPI check for available surveys
+     * returns. The argument will be null if no survey was available, or
+     * a non-null survey if one exists to be shown to the user.
+     *
+     * foundSurvey() should be safe to call from an arbitrary thread.
+     */
+    public void foundSurvey(Survey s);
+}
