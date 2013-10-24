c124573317d8f116dd8f6e73dbfc694594e13ff6
Joe Bowers <joe@mixpanel.com>
2013-10-24 13:30:52 -0700

    Use resources for survey activity

--- src/com/mixpanel/android/surveys/SurveyActivity.java
+++ src/com/mixpanel/android/surveys/SurveyActivity.java
@@ -4,6 +4,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
@@ -11,22 +12,26 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.database.DataSetObserver;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.RelativeLayout;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.mixpanel.android.R;
 import com.mixpanel.android.mpmetrics.MixpanelAPI;
 import com.mixpanel.android.mpmetrics.Survey;
+
+
 
 public class SurveyActivity extends Activity {
 
@@ -34,37 +39,35 @@ public class SurveyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String surveyJsonStr = getIntent().getStringExtra("surveyJson");
         mDistinctId = getIntent().getStringExtra("distinctId");
         mToken = getIntent().getStringExtra("token");
+        String surveyJsonStr = getIntent().getStringExtra("surveyJson");
         final byte[] backgroundJpgBytes = getIntent().getByteArrayExtra("backgroundJpgBytes");
-        mBackground = BitmapFactory.decodeByteArray(backgroundJpgBytes, 0, backgroundJpgBytes.length);
-        mMixpanel = MixpanelAPI.getInstance(this, mToken); // TODO CANT DO THIS. You've gotta make sure you use the same instance? But threads?
-
-        getWindow().setBackgroundDrawable(new BitmapDrawable(getResources(), mBackground));
-
+        final Bitmap background = BitmapFactory.decodeByteArray(backgroundJpgBytes, 0, backgroundJpgBytes.length);
+        getWindow().setBackgroundDrawable(new BitmapDrawable(getResources(), background));
+        setContentView(R.layout.com_mixpanel_android_activity_survey);
+        mProgressTextView = (TextView) findViewById(R.id.progress_text);
+        mPromptView = (TextView) findViewById(R.id.prompt_text);
+        mChoiceView = (ListView) findViewById(R.id.choice_list);
+        mChoiceView.setOnItemClickListener(new OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                String answer = parent.getItemAtPosition(position).toString();
+                saveAnswer(mCurrentQuestion, answer);
+                goToNextQuestion();
+            }
+        });
 
         // identify the person we're saving answers for TODO RACE CONDITION NEED DIRECT INSTANCE LOOKUP
+        mMixpanel = MixpanelAPI.getInstance(this, mToken); // TODO CANT DO THIS. You've gotta make sure you use the same instance? But threads?
         mMixpanel.getPeople().identify(mDistinctId);
-
         try {
             mSurvey = new Survey(new JSONObject(surveyJsonStr));
             mAnswers = new HashMap<Survey.Question, String>();
         } catch (JSONException e) {
             // TODO can't merge without doing something useful here.
-            Log.e(LOGTAG, "Unable to parse survey json: "+surveyJsonStr, e);
+            Log.e(LOGTAG, "Unable to parse survey json: " + surveyJsonStr, e);
         }
-
-        LinearLayout layout = constructView();
-        setContentView(layout);
-
-        // listen on skip button press
-        mSkipButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToNextQuestion();
-            }
-        });
 
         // TODO For testing only
         // mMixpanel.getPeople().append("$surveys", mSurvey.getId());
@@ -73,50 +76,12 @@ public class SurveyActivity extends Activity {
         showQuestion(0);
     }
 
-    private LinearLayout constructView() {
-        // top level layout
-        LinearLayout decideLayout = new LinearLayout(this);
-        decideLayout.setOrientation(LinearLayout.VERTICAL);
-        decideLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-
-        // relative layout for progress / skip btn container
-        RelativeLayout progressLayout = new RelativeLayout(this);
-        progressLayout.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
-
-        // progress text (e.g. Question 1 of 10)
-        mProgressTextView = new TextView(this);
-
-        // the question text
-        mPromptView = new TextView(this);
-        mPromptView.setTextSize(20);
-
-        // skip btn
-        mSkipButton = new Button(this);
-        mSkipButton.setText("Skip");
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        mSkipButton.setLayoutParams(params);
-        mSkipButton.setPadding(30, 10, 30, 10);
-
-        // radio group for answer choices
-        mAnswerRadioGroup = new RadioGroup(this);
-        mAnswerRadioGroup.setLayoutParams(new RadioGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-
-        // add em all
-        progressLayout.addView(mSkipButton);
-        progressLayout.addView(mProgressTextView);
-        decideLayout.addView(progressLayout);
-        decideLayout.addView(mPromptView);
-        decideLayout.addView(mAnswerRadioGroup);
-        return decideLayout;
-    }
-
     @Override
     public void onBackPressed() {
         if (mCurrentQuestion > 0) {
             showQuestion(mCurrentQuestion-1);
         } else {
-            super.onBackPressed();    //To change body of overridden methods use File | Settings | File Templates.
+            super.onBackPressed();
         }
     }
 
@@ -129,33 +94,15 @@ public class SurveyActivity extends Activity {
     }
 
     private void showQuestion(int idx) {
-
         Survey.Question question = mSurvey.getQuestions().get(idx);
         mCurrentQuestion = idx;
         mProgressTextView.setText("Question " + (idx + 1) + " of " + mSurvey.getQuestions().size());
         mPromptView.setText(question.getPrompt());
-        mAnswerRadioGroup.removeAllViews();
-        mAnswerRadioGroup.setOnCheckedChangeListener(null);
-
-        for (String choice:question.getChoices()) {
-            addChoice(question, choice);
-        }
-
-        mAnswerRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                RadioButton checkedButton = (RadioButton) findViewById(radioGroup.getCheckedRadioButtonId());
-                Object answer = checkedButton.getText();
-                saveAnswer(mCurrentQuestion, answer);
-                goToNextQuestion();
-            }
-        });
+        final ChoiceAdapter answerAdapter = new ChoiceAdapter(question.getChoices(), getLayoutInflater());
+        mChoiceView.setAdapter(answerAdapter);
     }
 
-    private void saveAnswer(int questionIdx, Object answer) {
-        Log.i(LOGTAG, "Answering question " + (questionIdx + 1) + ": " + answer);
-
-        // assign the answer to the question locally
+    private void saveAnswer(int questionIdx, String answer) {
         Survey.Question question = mSurvey.getQuestions().get(questionIdx);
         mAnswers.put(question, answer.toString());
         mMixpanel.getPeople().append("$responses", mSurvey.getCollectionId()); // <<--- TODO should be $union
@@ -180,18 +127,6 @@ public class SurveyActivity extends Activity {
         mMixpanel.flush();
     }
 
-    private void addChoice(Survey.Question question, Object choice) {
-        RadioButton radioButton = new RadioButton(this);
-        radioButton.setText((String)choice);
-        radioButton.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        radioButton.setPadding(50, 10, 0, 0);
-        mAnswerRadioGroup.addView(radioButton);
-
-        if (mAnswers.containsKey(question) && mAnswers.get(question).equals(choice)) {
-            mAnswerRadioGroup.check(radioButton.getId());
-        }
-    }
-
     private void completeSurvey() {
         finish();
     }
@@ -202,6 +137,82 @@ public class SurveyActivity extends Activity {
         super.onDestroy();
     }
 
+    private static class ChoiceAdapter implements ListAdapter {
+
+        public ChoiceAdapter(List<String> choices, LayoutInflater inflater) {
+            mChoices = choices;
+            mInflater = inflater;
+        }
+
+        @Override
+        public int getCount() {
+            return mChoices.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mChoices.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (null == convertView) {
+                convertView = mInflater.inflate(R.layout.com_mixpanel_android_multiple_choice_answer, parent, false);
+            }
+            TextView choiceText = (TextView) convertView.findViewById(R.id.multiple_choice_answer_text);
+            String choice = mChoices.get(position);
+            choiceText.setText(choice);
+            return convertView;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return mChoices.isEmpty();
+        }
+
+        @Override
+        public void registerDataSetObserver(DataSetObserver observer) {
+            ; // Underlying data *never* changes
+        }
+
+        @Override
+        public void unregisterDataSetObserver(DataSetObserver observer) {
+            ; // Underlying data never changes
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled(int arg0) {
+            return true;
+        }
+
+        private final List<String> mChoices;
+        private final LayoutInflater mInflater;
+    }
 
     private static final String LOGTAG = "MixpanelAPI";
     private MixpanelAPI mMixpanel;
@@ -210,9 +221,7 @@ public class SurveyActivity extends Activity {
     private String mToken;
     private TextView mPromptView;
     private TextView mProgressTextView;
-    private Button mSkipButton;
-    private RadioGroup mAnswerRadioGroup;
-    private Bitmap mBackground;
+    private ListView mChoiceView;
 
     private Map<Survey.Question, String> mAnswers;
     private int mCurrentQuestion = 0;
