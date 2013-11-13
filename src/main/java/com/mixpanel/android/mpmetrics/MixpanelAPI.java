330371e0e7364e2d13231712624331005a8e35c5
jbwyme <josh@mixpanel.com>
2013-11-13 11:22:16 -0800

    use manual versioning, push to nexus staging when version doesn't contain SNAPSHOT

--- src/main/java/com/mixpanel/android/mpmetrics/MixpanelAPI.java
+++ src/main/java/com/mixpanel/android/mpmetrics/MixpanelAPI.java
@@ -100,7 +100,7 @@ import com.mixpanel.android.util.StackBlurManager;
  * @see <a href="https://github.com/mixpanel/sample-android-mixpanel-integration">The Mixpanel Android sample application</a>
  */
 public class MixpanelAPI {
-    public static final String VERSION = "3.3.2";
+    public static final String VERSION = "4.0.0-SNAPSHOT";
 
     /**
      * You shouldn't instantiate MixpanelAPI objects directly.
