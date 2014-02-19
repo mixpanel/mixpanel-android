c66ab0b3b4eeca405e315ca16468f032264a4b8d
Anlu Wang <anlu@Anlus-MacBook-Pro.local>
2014-02-19 14:54:15 -0800

    Add mini notification view

--- src/main/java/com/mixpanel/android/mpmetrics/NotificationActivity.java
+++ src/main/java/com/mixpanel/android/mpmetrics/NotificationActivity.java
@@ -11,21 +11,30 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class NotificationActivity extends Activity {
-	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.com_mixpanel_android_activity_notification);
+		
+		if (getIntent().getIntExtra("notificationType", 0) == NOTIFICATION_TYPE_FULL) {
+	    	getWindow().setBackgroundDrawableResource(R.color.com_mixpanel_android_notification_dim);
+			setContentView(R.layout.com_mixpanel_android_activity_notification_full);
+
+			TextView subtextView = (TextView) findViewById(R.id.com_mixpanel_android_notification_subtext);
+			String subtextText = getIntent().getStringExtra("notificationSubtext");
+			subtextView.setText(subtextText);
+		} else {
+			getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+			setContentView(R.layout.com_mixpanel_android_activity_notification_mini);
+		}
 		
 		TextView titleView = (TextView) findViewById(R.id.com_mixpanel_android_notification_title);
 		String titleText = getIntent().getStringExtra("notificationTitle");
 		titleView.setText(titleText);
-		
-		TextView subtextView = (TextView) findViewById(R.id.com_mixpanel_android_notification_subtext);
-		String subtextText = getIntent().getStringExtra("notificationSubtext");
-		subtextView.setText(subtextText);
 	}
 
 	public void dismissNotification(final View view) {
 		finish();
 	}
+	
+	public static final int NOTIFICATION_TYPE_FULL = 0;
+	public static final int NOTIFICATION_TYPE_MINI = 1;
 }
