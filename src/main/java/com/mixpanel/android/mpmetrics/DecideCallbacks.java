71ccabf29ad3e708dddc447b43cf70a88d5054b1
Joe Bowers <joe@mixpanel.com>
2014-03-12 11:31:18 -0700

    Remove dead classes

--- src/main/java/com/mixpanel/android/mpmetrics/DecideCallbacks.java
+++
@@ -1,7 +0,0 @@
-package com.mixpanel.android.mpmetrics;
-
-import java.util.List;
-
-/* package */ interface DecideCallbacks {
-    public void foundResults(List<Survey> surveys, List<InAppNotification> notifications);
-}
