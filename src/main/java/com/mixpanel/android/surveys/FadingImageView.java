a3ff0af528e9cc927312c8966107d5b76dc88a73
Anlu Wang <anlu@mixpanel.com>
2014-03-31 20:23:08 -0700

    Add a fade to the bottom of takeover in app notifications

--- src/main/java/com/mixpanel/android/surveys/FadingImageView.java
+++ src/main/java/com/mixpanel/android/surveys/FadingImageView.java
@@ -1,6 +1,8 @@
 package com.mixpanel.android.surveys;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.LinearGradient;
 import android.graphics.Matrix;
@@ -10,7 +12,11 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.Shader;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
 import android.widget.ImageView;
+
+import com.mixpanel.android.R;
 
 
 public class FadingImageView extends ImageView {
@@ -30,16 +36,49 @@ public class FadingImageView extends ImageView {
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        mHeight = getHeight();
+        mWidth = getWidth();
+        int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // For Portrait takeover notifications, we have to fade out into the notification text
+            // at the bottom of the screen.
+
+            final View root = (View) this.getRootView();
+            final View bottomWrapperView = (View) root.findViewById(R.id.com_mixpanel_android_notification_bottom_wrapper);
+
+            // bottomWrapperView should have been measured already, so it's height should exist
+            // Still, guard against potential weird black magic rendering issues.
+            int bottomWrapperHeight = 0;
+            if (null != bottomWrapperView && bottomWrapperView.getHeight() != 0) {
+                bottomWrapperHeight = bottomWrapperView.getHeight();
+            }
+
+            // We don't want the fade out to end right at the beginning of the text, so we give it
+            // give it a few extra dp's of room.
+            Resources r = getResources();
+            float extraPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 15, r.getDisplayMetrics());
+            mGradientMatrix.setScale(1, parentHeight - bottomWrapperHeight + extraPx);
+        } else {
+            mGradientMatrix.setScale(1, parentHeight);
+        }
+        mGradientShader.setLocalMatrix(mGradientMatrix);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        // We have to override this low level draw method instead of onDraw, because by the time
+        // onDraw is called, the Canvas with the background has already been saved, so we can't
+        // actually clear it with our opacity gradient.
         final Rect clip = canvas.getClipBounds();
         final int restoreTo = canvas.saveLayer(0, 0, clip.width(), clip.height(), null, Canvas.ALL_SAVE_FLAG);
-        super.onDraw(canvas);
 
-        final int height = getHeight();
-        final int width = getWidth();
-        mGradientMatrix.setScale(1, height);
-        mGradientShader.setLocalMatrix(mGradientMatrix);
-        canvas.drawRect(0, 0, width, height, mGradientPaint);
+        super.draw(canvas);
+
+        canvas.drawRect(0, 0, mWidth, mHeight, mGradientPaint);
         canvas.restoreToCount(restoreTo);
     }
 
@@ -56,10 +95,11 @@ public class FadingImageView extends ImageView {
 
         mGradientPaint.setShader(mGradientShader);
         mGradientPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-
     }
 
     private Paint mGradientPaint;
     private Matrix mGradientMatrix;
     private Shader mGradientShader;
+    private int mHeight;
+    private int mWidth;
 }
