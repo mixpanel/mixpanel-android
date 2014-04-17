bcddbae422155a159351036a6b55554c6e1ef7ab
Anlu Wang <anlu@Anlus-MacBook-Pro.local>
2014-04-16 18:28:05 -0700

    Final version number update

--- src/main/java/com/mixpanel/android/mpmetrics/MPConfig.java
+++ src/main/java/com/mixpanel/android/mpmetrics/MPConfig.java
@@ -11,7 +11,7 @@ import android.util.Log;
  * Stores global configuration options for the Mixpanel library.
  */
 public class MPConfig {
-    public static final String VERSION = "4.1.0-RC1";
+    public static final String VERSION = "4.1.0";
 
     public static boolean DEBUG = false;
 
