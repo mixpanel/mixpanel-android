f376568ae18f7d98f0a2bdaa2dd690aaaddee317
jbwyme <josh@mixpanel.com>
2014-06-27 13:57:34 -0700

    some refactoring and added tests for Tweaks

--- src/main/java/com/mixpanel/android/mpmetrics/ABTesting.java
+++ src/main/java/com/mixpanel/android/mpmetrics/ABTesting.java
@@ -15,6 +15,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.mixpanel.android.abtesting.Tweaks;
+
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 import org.json.JSONArray;
@@ -49,6 +51,10 @@ public class ABTesting implements Application.ActivityLifecycleCallbacks {
             mHandler = new ABHandler(thread.getLooper());
             mHandler.sendMessage(mHandler.obtainMessage(MESSAGE_CONNECT_TO_PROXY));
         }
+    }
+
+    public Tweaks getTweaks() {
+        return mTweaks;
     }
 
     void handleChangesReceived(JSONObject changes, boolean persist, boolean applyToLive) {
@@ -224,65 +230,65 @@ public class ABTesting implements Application.ActivityLifecycleCallbacks {
             }
         }
 
-    private JSONObject serializeView(View view, int index) throws IOException {
-        final JSONObject dump = new JSONObject();
-        try {
-            dump.put("hashCode", view.hashCode());
-            dump.put("id", view.getId());
-            dump.put("tag", view.getTag());
+        private JSONObject serializeView(View view, int index) throws IOException {
+            final JSONObject dump = new JSONObject();
+            try {
+                dump.put("hashCode", view.hashCode());
+                dump.put("id", view.getId());
+                dump.put("tag", view.getTag());
 
-            dump.put("top", view.getTop());
-            dump.put("left", view.getLeft());
-            dump.put("width", view.getWidth());
-            dump.put("height", view.getHeight());
+                dump.put("top", view.getTop());
+                dump.put("left", view.getLeft());
+                dump.put("width", view.getWidth());
+                dump.put("height", view.getHeight());
 
-            final JSONArray classes = new JSONArray();
-            Class klass = view.getClass();
-            do {
-                classes.put(klass.getName());
-                klass = klass.getSuperclass();
-            } while (klass != Object.class);
-            dump.put("classes", classes);
+                final JSONArray classes = new JSONArray();
+                Class klass = view.getClass();
+                do {
+                    classes.put(klass.getName());
+                    klass = klass.getSuperclass();
+                } while (klass != Object.class);
+                dump.put("classes", classes);
 
-            JSONArray methodList = new JSONArray();
-            for (Method m : view.getClass().getMethods()) {
-                JSONObject method = new JSONObject();
-                if (m.getName().startsWith("set")) {
-                    JSONArray argTypes = new JSONArray();
-                    for (Class c : m.getParameterTypes()) {
-                        argTypes.put(c.getCanonicalName());
+                JSONArray methodList = new JSONArray();
+                for (Method m : view.getClass().getMethods()) {
+                    JSONObject method = new JSONObject();
+                    if (m.getName().startsWith("set")) {
+                        JSONArray argTypes = new JSONArray();
+                        for (Class c : m.getParameterTypes()) {
+                            argTypes.put(c.getCanonicalName());
+                        }
+
+                        method.put("name", m.getName());
+                        method.put("args", argTypes);
+                        methodList.put(method);
                     }
-
-                    method.put("name", m.getName());
-                    method.put("args", argTypes);
-                    methodList.put(method);
                 }
-            }
-            dump.put("methods", methodList);
+                dump.put("methods", methodList);
 
-            JSONArray children = new JSONArray();
-            if (view instanceof ViewGroup) {
-                final Map<String, Integer> viewIndex = new HashMap<String, Integer>();
-                final ViewGroup group = (ViewGroup) view;
-                final int childCount = group.getChildCount();
+                JSONArray children = new JSONArray();
+                if (view instanceof ViewGroup) {
+                    final Map<String, Integer> viewIndex = new HashMap<String, Integer>();
+                    final ViewGroup group = (ViewGroup) view;
+                    final int childCount = group.getChildCount();
 
-                for (int i = 0; i < childCount; i++) {
-                    final View child = group.getChildAt(i);
-                    int childIndex = 1;
-                    if (viewIndex.containsKey(child.getClass().getCanonicalName())) {
-                        childIndex = viewIndex.get(child.getClass().getCanonicalName()) + 1;
+                    for (int i = 0; i < childCount; i++) {
+                        final View child = group.getChildAt(i);
+                        int childIndex = 1;
+                        if (viewIndex.containsKey(child.getClass().getCanonicalName())) {
+                            childIndex = viewIndex.get(child.getClass().getCanonicalName()) + 1;
+                        }
+                        viewIndex.put(child.getClass().getCanonicalName(), childIndex);
+                        children.put(serializeView(child, childIndex));
                     }
-                    viewIndex.put(child.getClass().getCanonicalName(), childIndex);
-                    children.put(serializeView(child, childIndex));
                 }
+                dump.put("children", children);
+                dump.put("index", index);
+            } catch (JSONException impossible) {
+                throw new RuntimeException("Apparently Impossible JSONException", impossible);
             }
-            dump.put("children", children);
-            dump.put("index", index);
-        } catch (JSONException impossible) {
-            throw new RuntimeException("Apparently Impossible JSONException", impossible);
+            return dump;
         }
-        return dump;
-    }
 
         private ProxyClient mProxyClient;
         private List<Activity> liveActivities = new ArrayList<Activity>();
@@ -377,119 +383,12 @@ public class ABTesting implements Application.ActivityLifecycleCallbacks {
         private final byte[] mBuffer;
     }
 
-
-    /**
-     * Tweaks allows applications to specify dynamic variables that can be modified in the Mixpanel UI
-     * and reflected in the app.
-     *
-     * Example (assignment):
-     *              String welcomeMsg = getTweaks().getString("welcome message", "Welcome to the app!");
-     *
-     *
-     * Example (callback):
-     *              final Button mButton = (Button) findViewById(R.id.button2);
-     *              getTweaks().bind("Start button", "Click to start!", new ABTesting.TweakChangeCallback() {
-     *                  @Override
-     *                  public void onChange(Object o) {
-     *                      mButton.setText((String) o);
-     *                  }
-     *              });
-     *
-     */
-    public class Tweaks {
-        Map<String, Object> tweaks = new HashMap<String, Object>();
-        Map<String, List<TweakChangeCallback>> bindings = new HashMap<String, List<TweakChangeCallback>>();
-
-        public String getString(String tweakName, String defaultValue) {
-            return (String) get(tweakName, defaultValue);
-        }
-
-        public Integer getInteger(String tweakName, Integer defaultValue) {
-            return (Integer) get(tweakName, defaultValue);
-        }
-
-        public Long getLong(String tweakName, Long defaultValue) {
-            return (Long) get(tweakName, defaultValue);
-        }
-
-        public Float getFloat(String tweakName, Float defaultValue) {
-            return (Float) get(tweakName, defaultValue);
-        }
-
-        public Double getDouble(String tweakName, Double defaultValue) {
-            return (Double) get(tweakName, defaultValue);
-        }
-
-        public Object get(String tweakName, Object defaultValue) {
-            Object value;
-            synchronized (tweaks) {
-                if (!tweaks.containsKey(tweakName)) {
-                    set(tweakName, defaultValue, true);
-                }
-                value = tweaks.get(tweakName);
-            }
-            return value;
-        }
-
-        public Map<String, Object> getAll() {
-            HashMap<String, Object> copy;
-            synchronized (tweaks) {
-                copy = new HashMap<String, Object>(tweaks);
-            }
-            return copy;
-        }
-
-        public void bind(String tweakName, Object defaultValue, TweakChangeCallback callback) {
-            synchronized (bindings) {
-                if (!bindings.containsKey(tweakName)) {
-                    bindings.put(tweakName, new ArrayList<TweakChangeCallback>());
-                }
-                bindings.get(tweakName).add(callback);
-            }
-            callback.onChange(get(tweakName, defaultValue));
-        }
-
-        public void set(String tweakName, Object value) {
-            set(tweakName, value, false);
-        }
-
-        public void set(Map<String, Object> tweakUpdates) {
-            for(String tweakName : tweakUpdates.keySet()) {
-                set(tweakName, tweakUpdates.get(tweakName));
-            }
-        }
-
-        private void set(String tweakName, Object value, boolean isDefault) {
-            synchronized (tweaks) {
-                tweaks.put(tweakName, value);
-            }
-
-            synchronized (bindings) {
-                if (!isDefault && bindings.containsKey(tweakName)) {
-                    for(TweakChangeCallback changeCallback : bindings.get(tweakName)) {
-                        changeCallback.onChange(value); // todo: should we run this on the UIThread?
-                    }
-                }
-            }
-        }
-    }
-
-    public interface TweakChangeCallback {
-        public void onChange(Object value);
-    }
-
-    public Tweaks getTweaks() {
-        return mTweaks;
-    }
-
     private ABHandler mHandler;
-    private Tweaks mTweaks = new Tweaks();
-
+    private final Tweaks mTweaks = new Tweaks();
     private final Context mContext;
     private static final String PROXY_URL = "ws://anluswang.com/websocket_proxy/THE_KEY";
     private static final int MESSAGE_CONNECT_TO_PROXY = 0;
     private static final int MESSAGE_SEND_STATE_FOR_EDITING = 1;
     private static final int MESSAGE_HANDLE_CHANGES_RECEIVED = 2;
     private static final String LOGTAG = "ABTesting";
-
 }
