22a57f1f343d45b4af6b4d39b1007376714c8d44
jbwyme <josh@mixpanel.com>
2014-06-27 12:46:13 -0700

    adding Tweaks

--- src/main/java/com/mixpanel/android/mpmetrics/ABTesting.java
+++ src/main/java/com/mixpanel/android/mpmetrics/ABTesting.java
@@ -100,6 +100,7 @@ public class ABTesting implements Application.ActivityLifecycleCallbacks {
     @Override
     public void onActivityDestroyed(Activity activity) { }
 
+
     /**
      * This class is really the main class for ABTesting. It does all the work on a HandlerThread.
      */
@@ -376,7 +377,113 @@ public class ABTesting implements Application.ActivityLifecycleCallbacks {
         private final byte[] mBuffer;
     }
 
+
+    /**
+     * Tweaks allows applications to specify dynamic variables that can be modified in the Mixpanel UI
+     * and reflected in the app.
+     *
+     * Example (assignment):
+     *              String welcomeMsg = getTweaks().getString("welcome message", "Welcome to the app!");
+     *
+     *
+     * Example (callback):
+     *              final Button mButton = (Button) findViewById(R.id.button2);
+     *              getTweaks().bind("Start button", "Click to start!", new ABTesting.TweakChangeCallback() {
+     *                  @Override
+     *                  public void onChange(Object o) {
+     *                      mButton.setText((String) o);
+     *                  }
+     *              });
+     *
+     */
+    public class Tweaks {
+        Map<String, Object> tweaks = new HashMap<String, Object>();
+        Map<String, List<TweakChangeCallback>> bindings = new HashMap<String, List<TweakChangeCallback>>();
+
+        public String getString(String tweakName, String defaultValue) {
+            return (String) get(tweakName, defaultValue);
+        }
+
+        public Integer getInteger(String tweakName, Integer defaultValue) {
+            return (Integer) get(tweakName, defaultValue);
+        }
+
+        public Long getLong(String tweakName, Long defaultValue) {
+            return (Long) get(tweakName, defaultValue);
+        }
+
+        public Float getFloat(String tweakName, Float defaultValue) {
+            return (Float) get(tweakName, defaultValue);
+        }
+
+        public Double getDouble(String tweakName, Double defaultValue) {
+            return (Double) get(tweakName, defaultValue);
+        }
+
+        public Object get(String tweakName, Object defaultValue) {
+            Object value;
+            synchronized (tweaks) {
+                if (!tweaks.containsKey(tweakName)) {
+                    set(tweakName, defaultValue, true);
+                }
+                value = tweaks.get(tweakName);
+            }
+            return value;
+        }
+
+        public Map<String, Object> getAll() {
+            HashMap<String, Object> copy;
+            synchronized (tweaks) {
+                copy = new HashMap<String, Object>(tweaks);
+            }
+            return copy;
+        }
+
+        public void bind(String tweakName, Object defaultValue, TweakChangeCallback callback) {
+            synchronized (bindings) {
+                if (!bindings.containsKey(tweakName)) {
+                    bindings.put(tweakName, new ArrayList<TweakChangeCallback>());
+                }
+                bindings.get(tweakName).add(callback);
+            }
+            callback.onChange(get(tweakName, defaultValue));
+        }
+
+        public void set(String tweakName, Object value) {
+            set(tweakName, value, false);
+        }
+
+        public void set(Map<String, Object> tweakUpdates) {
+            for(String tweakName : tweakUpdates.keySet()) {
+                set(tweakName, tweakUpdates.get(tweakName));
+            }
+        }
+
+        private void set(String tweakName, Object value, boolean isDefault) {
+            synchronized (tweaks) {
+                tweaks.put(tweakName, value);
+            }
+
+            synchronized (bindings) {
+                if (!isDefault && bindings.containsKey(tweakName)) {
+                    for(TweakChangeCallback changeCallback : bindings.get(tweakName)) {
+                        changeCallback.onChange(value); // todo: should we run this on the UIThread?
+                    }
+                }
+            }
+        }
+    }
+
+    public interface TweakChangeCallback {
+        public void onChange(Object value);
+    }
+
+    public Tweaks getTweaks() {
+        return mTweaks;
+    }
+
     private ABHandler mHandler;
+    private Tweaks mTweaks = new Tweaks();
 
     private final Context mContext;
     private static final String PROXY_URL = "ws://anluswang.com/websocket_proxy/THE_KEY";
@@ -384,4 +491,5 @@ public class ABTesting implements Application.ActivityLifecycleCallbacks {
     private static final int MESSAGE_SEND_STATE_FOR_EDITING = 1;
     private static final int MESSAGE_HANDLE_CHANGES_RECEIVED = 2;
     private static final String LOGTAG = "ABTesting";
+
 }
