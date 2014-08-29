556f1ba5e211484c59759761ecb9a93e052135d2
Joe Bowers <joe@mixpanel.com>
2014-08-29 14:56:09 -0700

    Missing asset

---
+++ src/main/java/com/mixpanel/android/abtesting/PropertyDescription.java
@@ -0,0 +1,28 @@
+package com.mixpanel.android.abtesting;
+
+/**
+* Created by joe on 8/29/14.
+*/
+/* package */ class PropertyDescription {
+    public PropertyDescription(String name, Class targetClass, Caller accessor, String mutatorName) {
+        this.name = name;
+        this.targetClass = targetClass;
+        this.accessor = accessor;
+
+        mMutatorName = mutatorName;
+    }
+
+    public Caller makeMutator(Object[] methodArgs) {
+        if (null == mMutatorName) {
+            return null;
+        }
+
+        return new Caller(mMutatorName, methodArgs, Void.TYPE);
+    }
+
+    public final String name;
+    public final Class targetClass;
+    public final Caller accessor;
+
+    private final String mMutatorName;
+}
