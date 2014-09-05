bef5bd2bfecf4f2d0665a6bcafdf2ed2a2e99911
Joe Bowers <joe@mixpanel.com>
2014-09-05 10:10:43 -0700

    Remove annoying "Created by" comment

--- src/main/java/com/mixpanel/android/viewcrawler/PropertyDescription.java
+++ src/main/java/com/mixpanel/android/viewcrawler/PropertyDescription.java
@@ -1,8 +1,6 @@
 package com.mixpanel.android.viewcrawler;
 
-/**
-* Created by joe on 8/29/14.
-*/
+
 /* package */ class PropertyDescription {
     public PropertyDescription(String name, Class targetClass, Caller accessor, String mutatorName) {
         this.name = name;
