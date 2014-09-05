21f9fd6a6ef7354165e3e4d2d8f90b7831be97ca
Joe Bowers <joe@mixpanel.com>
2014-09-05 13:02:08 -0700

    Hide internal public junk from Javadoc

--- src/main/java/com/mixpanel/android/mpmetrics/InAppNotification.java
+++ src/main/java/com/mixpanel/android/mpmetrics/InAppNotification.java
@@ -12,7 +12,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Represents a fullscreen in-app notification delivered from Mixpanel.
+ * Represents a in-app notification delivered from Mixpanel.
  */
 public class InAppNotification implements Parcelable {
     public enum Type {
@@ -53,7 +53,7 @@ public class InAppNotification implements Parcelable {
         mCallToAction = in.readString();
         mCallToActionUrl = in.readString();
 
-        mImage = (Bitmap) in.readParcelable(Bitmap.class.getClassLoader());
+        mImage = in.readParcelable(Bitmap.class.getClassLoader());
     }
 
     /* package */ InAppNotification(JSONObject description) throws BadDecideObjectException {
