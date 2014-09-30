f61a94f0ced1b010bf95a14958bf19ac27f56388
Joe Bowers <joe@mixpanel.com>
2014-09-30 12:27:25 -0700

    Missing asset

---
+++ src/main/java/com/mixpanel/android/viewcrawler/UpdatesFromMixpanel.java
@@ -0,0 +1,10 @@
+package com.mixpanel.android.viewcrawler;
+
+import org.json.JSONArray;
+
+/* This interface is for internal use in the Mixpanel library, and should not be
+   implemented in client code. */
+public interface UpdatesFromMixpanel {
+    public void setEventBindings(JSONArray bindings);
+    public Tweaks getTweaks();
+}
