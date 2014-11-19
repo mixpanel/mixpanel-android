4c73d2f7e047ad489677e149482dac7fb7195767
Joe Bowers <joe@mixpanel.com>
2014-11-19 10:06:53 -0800

    New scheme for scheduling debounce tasks, account for real time w/ debounced events

--- src/main/java/com/mixpanel/android/viewcrawler/DynamicEventTracker.java
+++ src/main/java/com/mixpanel/android/viewcrawler/DynamicEventTracker.java
@@ -15,6 +15,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * Handles translating events detected by ViewVisitors into events sent to Mixpanel
+ *
+ * - Builds properties by interrogating view subtrees
+ *
+ * - Possibly debounces events using the Handler given at construction
+ *
+ * - Calls MixpanelAPI.track
+ */
 /* package */ class DynamicEventTracker implements ViewVisitor.OnEventListener {
 
     public DynamicEventTracker(MixpanelAPI mixpanel, Handler homeHandler) {
@@ -26,34 +35,48 @@ import java.util.Map;
 
     @Override
     public void OnEvent(View v, String eventName, boolean debounce) {
+        // Will be called on the UI thread
+        final long moment = System.currentTimeMillis();
         final JSONObject properties = new JSONObject();
         try {
             final String text = textPropertyFromView(v);
             properties.put("mp_text", text);
+
+            // We may call track much later, but we'll be tracking something
+            // that happened right at moment.
+            properties.put("time", moment / 1000);
         } catch (JSONException e) {
             Log.e(LOGTAG, "Can't format properties from view due to JSON issue", e);
         }
 
         if (debounce) {
             final Signature eventSignature = new Signature(v, eventName);
-            final UnsentEvent event = new UnsentEvent(eventName, properties, System.currentTimeMillis());
+            final UnsentEvent event = new UnsentEvent(eventName, properties, moment);
+
+            // No scheduling mTask without holding a lock on mDebouncedEvents,
+            // so that we don't have a rogue thread spinning away when no events
+            // are coming in.
             synchronized (mDebouncedEvents) {
+                final boolean needsRestart = mDebouncedEvents.isEmpty();
                 mDebouncedEvents.put(eventSignature, event);
-                mHandler.removeCallbacks(mTask);
-                mHandler.postDelayed(mTask, DEBOUNCE_TIME_MILLIS);
+                if (needsRestart) {
+                    mHandler.postDelayed(mTask, DEBOUNCE_TIME_MILLIS);
+                }
             }
         } else {
             mMixpanel.track(eventName, properties);
         }
     }
 
+    // Attempts to send all tasks in mDebouncedEvents that have been waiting for
+    // more than DEBOUNCE_TIME_MILLIS. Will reschedule itself as long as there
+    // are more events waiting (but will *not* wait on an empty set)
     private final class SendDebouncedTask implements Runnable {
         @Override
         public void run() {
             final long now = System.currentTimeMillis();
             synchronized (mDebouncedEvents) {
                 final Iterator<Map.Entry<Signature, UnsentEvent>> iter = mDebouncedEvents.entrySet().iterator();
-
                 while (iter.hasNext()) {
                     final Map.Entry<Signature, UnsentEvent> entry = iter.next();
                     final UnsentEvent val = entry.getValue();
@@ -64,6 +87,7 @@ import java.util.Map;
                 }
 
                 if (! mDebouncedEvents.isEmpty()) {
+                    // In the average case, this is enough time to catch the next signal
                     mHandler.postDelayed(this, DEBOUNCE_TIME_MILLIS / 2);
                 }
             } // synchronized
@@ -110,6 +134,8 @@ import java.util.Map;
         return ret;
     }
 
+    // An event is the same from a debouncing perspective if it comes from the same view,
+    // and has the same event name.
     private static class Signature {
         public Signature(final View view, final String eventName) {
             mHashCode = view.hashCode() ^ eventName.hashCode();
