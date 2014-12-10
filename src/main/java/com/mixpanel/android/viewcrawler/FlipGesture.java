77c1e4b1e909de9fd5852b5628f548343cc66225
Joe Bowers <joe@mixpanel.com>
2014-12-09 18:24:40 -0800

    Quicker, more responsive gesture

--- src/main/java/com/mixpanel/android/viewcrawler/FlipGesture.java
+++ src/main/java/com/mixpanel/android/viewcrawler/FlipGesture.java
@@ -20,18 +20,30 @@ import com.mixpanel.android.mpmetrics.MPConfig;
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        // Samples may come in around 4 times per second
+
         final float[] smoothed = smoothXYZ(event.values);
         final int oldFlipState = mFlipState;
 
-        if (smoothed[0] > MAXIMUM_XY_GRAVITY_FOR_FLIP || smoothed[0] < -MAXIMUM_XY_GRAVITY_FOR_FLIP) {
-            mFlipState = FLIP_STATE_NONE;
-        } else if (smoothed[1] > MAXIMUM_XY_GRAVITY_FOR_FLIP || smoothed[1] < -MAXIMUM_XY_GRAVITY_FOR_FLIP) {
-            mFlipState = FLIP_STATE_NONE;
-        } else if (smoothed[2] > MINIMUM_Z_GRAVITY_FOR_FLIP) {
+        final float totalGravitySquared =
+                smoothed[0] * smoothed[0] + smoothed[1] * smoothed[1] + smoothed[2] * smoothed[2];
+
+        final float minimumGravitySquared = MINIMUM_GRAVITY_FOR_FLIP * MINIMUM_GRAVITY_FOR_FLIP;
+        final float maximumGravitySquared = MAXIMUM_GRAVITY_FOR_FLIP * MAXIMUM_GRAVITY_FOR_FLIP;
+
+        mFlipState = FLIP_STATE_NONE;
+
+        if (smoothed[2] > MINIMUM_GRAVITY_FOR_FLIP && smoothed[2] < MAXIMUM_GRAVITY_FOR_FLIP) {
             mFlipState = FLIP_STATE_UP;
-        } else if (smoothed[2] < -MINIMUM_Z_GRAVITY_FOR_FLIP) {
+        }
+
+        if (smoothed[2] < -MINIMUM_GRAVITY_FOR_FLIP && smoothed[2] > -MAXIMUM_GRAVITY_FOR_FLIP) {
             mFlipState = FLIP_STATE_DOWN;
-        } else {
+        }
+
+        // Might overwrite current state, which is what we want.
+        if (totalGravitySquared < minimumGravitySquared ||
+                totalGravitySquared > maximumGravitySquared) {
             mFlipState = FLIP_STATE_NONE;
         }
 
@@ -39,26 +51,37 @@ import com.mixpanel.android.mpmetrics.MPConfig;
             mLastFlipTime = event.timestamp;
         }
 
+        // We need at least 1/4 seconds to recognize an UP or DOWN state
+        // We need at least 1 seconds to recognize a NONE state
+
         final long flipDurationNanos = event.timestamp - mLastFlipTime;
 
-        if (flipDurationNanos > MINIMUM_FLIP_DURATION_NANOS) {
-            if (mFlipState == FLIP_STATE_NONE && mTriggerState != TRIGGER_STATE_NONE) {
-                if (MPConfig.DEBUG) {
-                    Log.v(LOGTAG, "Flip gesture abandoned");
+        switch (mFlipState) {
+            case FLIP_STATE_DOWN:
+                if (flipDurationNanos > MINIMUM_UP_DOWN_DURATION && mTriggerState == TRIGGER_STATE_NONE) {
+                    if (MPConfig.DEBUG) {
+                        Log.v(LOGTAG, "Flip gesture begun");
+                    }
+                    mTriggerState = TRIGGER_STATE_BEGIN;
                 }
-                mTriggerState = TRIGGER_STATE_NONE;
-            } else if (mFlipState == FLIP_STATE_DOWN && mTriggerState == TRIGGER_STATE_NONE) {
-                if (MPConfig.DEBUG) {
-                    Log.v(LOGTAG, "Flip gesture begun");
+                break;
+            case FLIP_STATE_UP:
+                if (flipDurationNanos > MINIMUM_UP_DOWN_DURATION && mTriggerState == TRIGGER_STATE_BEGIN) {
+                    if (MPConfig.DEBUG) {
+                        Log.v(LOGTAG, "Flip gesture completed");
+                    }
+                    mTriggerState = TRIGGER_STATE_NONE;
+                    mListener.onFlipGesture();
                 }
-                mTriggerState = TRIGGER_STATE_BEGIN;
-            } else if (mFlipState == FLIP_STATE_UP && mTriggerState == TRIGGER_STATE_BEGIN) {
-                if (MPConfig.DEBUG) {
-                    Log.v(LOGTAG, "Flip gesture completed");
+                break;
+            case FLIP_STATE_NONE:
+                if (flipDurationNanos > MINIMUM_CANCEL_DURATION && mTriggerState != TRIGGER_STATE_NONE) {
+                    if (MPConfig.DEBUG) {
+                        Log.v(LOGTAG, "Flip gesture abandoned");
+                    }
+                    mTriggerState = TRIGGER_STATE_NONE;
                 }
-                mTriggerState = TRIGGER_STATE_NONE;
-                mListener.onFlipGesture();
-            }
+                break;
         }
     }
 
@@ -68,6 +91,7 @@ import com.mixpanel.android.mpmetrics.MPConfig;
     }
 
     private float[] smoothXYZ(final float[] samples) {
+        // Note that smoothing doesn't depend on sample timestamp!
         for (int i = 0; i < 3; i++) {
             final float oldVal = mSmoothed[i];
             mSmoothed[i] = oldVal + (ACCELEROMETER_SMOOTHING * (samples[i] - oldVal));
@@ -82,10 +106,13 @@ import com.mixpanel.android.mpmetrics.MPConfig;
     private final float[] mSmoothed = new float[3];
     private final OnFlipGestureListener mListener;
 
-    private static final float MINIMUM_Z_GRAVITY_FOR_FLIP = 9.0f;
-    private static final float MAXIMUM_XY_GRAVITY_FOR_FLIP = 2.0f;
+    private static final float MINIMUM_GRAVITY_FOR_FLIP = 9.8f - 2.0f;
+    private static final float MAXIMUM_GRAVITY_FOR_FLIP = 9.8f + 2.0f;
 
-    private static final long MINIMUM_FLIP_DURATION_NANOS = 1000000000;
+    // 1000000000 one second
+    //  250000000 one quarter second
+    private static final long MINIMUM_UP_DOWN_DURATION = 250000000;
+    private static final long MINIMUM_CANCEL_DURATION = 1000000000;
 
     private static final int FLIP_STATE_UP = -1;
     private static final int FLIP_STATE_NONE = 0;
@@ -94,7 +121,8 @@ import com.mixpanel.android.mpmetrics.MPConfig;
     private static final int TRIGGER_STATE_NONE = 0;
     private static final int TRIGGER_STATE_BEGIN = 1;
 
-    private static final float ACCELEROMETER_SMOOTHING = 0.5f;
+    // Higher is noisier but more responsive, 1.0 to 0.0
+    private static final float ACCELEROMETER_SMOOTHING = 0.7f;
 
     private static final String LOGTAG = "MixpanelAPI.FlipGesture";
 }
