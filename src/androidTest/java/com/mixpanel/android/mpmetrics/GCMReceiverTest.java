ff9ee56dea627a5b744075b5f31c35bd53beedfc
Joe Bowers <joe@mixpanel.com>
2014-12-24 10:24:40 -0800

    Test stubs to fill out before release

---
+++ src/androidTest/java/com/mixpanel/android/mpmetrics/GCMReceiverTest.java
@@ -0,0 +1,9 @@
+package com.mixpanel.android.mpmetrics;
+
+import android.test.AndroidTestCase;
+
+public class GCMReceiverTest extends AndroidTestCase {
+    public void testNotificationCreated() {
+        fail("Need to write this test");
+    }
+}
---
+++ src/androidTest/java/com/mixpanel/android/mpmetrics/ResourceReaderTest.java
@@ -0,0 +1,14 @@
+package com.mixpanel.android.mpmetrics;
+
+
+import android.test.AndroidTestCase;
+
+public class ResourceReaderTest extends AndroidTestCase {
+    public void testLocalResourceReader() {
+        fail("Need to write this test");
+    }
+
+    public void testSystemResourceReader() {
+        fail("Need to write this test");
+    }
+}
