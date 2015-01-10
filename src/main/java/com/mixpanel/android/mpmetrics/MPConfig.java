44a7af23796c260e64313277125bf43a14e56bfb
Joe Bowers <joe@mixpanel.com>
2015-01-09 16:17:29 -0800

    release version

--- src/main/java/com/mixpanel/android/mpmetrics/MPConfig.java
+++ src/main/java/com/mixpanel/android/mpmetrics/MPConfig.java
@@ -16,7 +16,7 @@ public class MPConfig {
     // Unfortunately, as long as we support building from source in Eclipse,
     // we can't rely on BuildConfig.MIXPANEL_VERSION existing, so this must
     // be hard-coded both in our gradle files and here in code.
-    public static final String VERSION = "4.5.2-SNAPSHOT";
+    public static final String VERSION = "4.5.2";
 
     public static boolean DEBUG = false;
 
