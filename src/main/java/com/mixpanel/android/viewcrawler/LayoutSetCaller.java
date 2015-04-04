d388a295a1c6d51c7e060acb1dcc602f269c40c5
Yinfei Ru <yinfei@mixpanel.com>
2015-04-03 17:35:36 -0700

    add files

---
+++ src/main/java/com/mixpanel/android/viewcrawler/LayoutSetCaller.java
@@ -0,0 +1,37 @@
+package com.mixpanel.android.viewcrawler;
+
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import org.json.JSONArray;
+
+/**
+ * Created by yinfeiru on 3/31/15.
+ */
+public class LayoutSetCaller {
+    public LayoutSetCaller(JSONArray args) {
+        if (args.length() == 1) {
+            mArgs = new int[]{args.optInt(RULE_INDEX), RelativeLayout.TRUE};
+        } else {
+            mArgs = new int[]{args.optInt(RULE_INDEX), args.optInt(ANCHOR_ID)};
+        }
+    }
+
+    public int[] getArgs() {
+        return mArgs;
+    }
+
+    public void applyMethod(View target) {
+        applyMethodWithArguments(target, mArgs);
+    }
+
+    public void applyMethodWithArguments(View target, int[] args) {
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)target.getLayoutParams();
+        params.addRule(args[RULE_INDEX], args[ANCHOR_ID]);
+        target.setLayoutParams(params);
+    }
+
+    private final int[] mArgs;
+    public static final int RULE_INDEX = 0;
+    public static final int ANCHOR_ID = 1;
+}
