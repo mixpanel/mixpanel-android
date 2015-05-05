0579b5b1174b3827543fe3085b7b202bf6fa8c82
Yinfei Ru <yinfei@mixpanel.com>
2015-05-05 09:57:38 -0700

    remove NonNull

--- src/main/java/com/mixpanel/android/mpmetrics/MixpanelAPI.java
+++ src/main/java/com/mixpanel/android/mpmetrics/MixpanelAPI.java
@@ -17,7 +17,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.mixpanel.android.R;
