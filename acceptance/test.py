96d213dee52c927ad728724645f236932ab1b783
Yinfei Ru <yinfei@mixpanel.com>
2015-05-06 13:09:36 -0700

    fix the text size test

--- acceptance/test.py
+++ acceptance/test.py
@@ -14,12 +14,13 @@ class AndroidTest(unittest.TestCase):
         self.driver.quit()
 
     def test_change_text_size(self):
-        text_size_change_response = '{"variants":[{"experiment_id":3585,"id":7647,"tweaks":[],"actions":[{"path":[{"id":16908290,"index":0,"prefix":"shortest"},{"view_class":"android.widget.LinearLayout","index":0},{"index":0,"mp_id_name":"edit_first_name"}],"args":[[4289362242,"java.lang.Integer"]],"property":{"get":{"selector":"getTextColors","result":{"type":"android.content.res.ColorStateList"},"parameters":[]},"set":{"selector":"setTextColor","parameters":[{"type":"java.lang.Integer"}]},"name":"textColor","classname":"android.widget.TextView"},"name":"c117"},{"path":[{"id":16908290,"index":0,"prefix":"shortest"},{"view_class":"android.widget.LinearLayout","index":0},{"index":0,"mp_id_name":"edit_first_name"}],"args":[[2,"java.lang.Integer"],[36,"java.lang.Float"]],"property":{"get":{"selector":"getTextSize","result":{"type":"java.lang.Float"},"parameters":[]},"set":{"selector":"setTextSize","parameters":[{"type":"java.lang.Integer"},{"type":"java.lang.Float"}]},"name":"fontSize","classname":"android.widget.TextView"},"name":"c131"}]}],"surveys":[],"notifications":[]}'
+        text_size_change_response = '{"notifications":[],"surveys":[],"variants":[{"tweaks":[],"actions":[{"property":{"classname":"android.widget.TextView","set":{"parameters":[{"type":"java.lang.Integer"}],"selector":"setTextColor"},"name":"textColor","get":{"result":{"type":"android.content.res.ColorStateList"},"parameters":[],"selector":"getTextColors"}},"path":[{"index":0,"prefix":"shortest","id":16908290},{"index":0,"view_class":"android.widget.LinearLayout"},{"index":0,"view_class":"android.widget.Button"}],"args":[[4278253824,"java.lang.Integer"]],"change_type":"property","name":"c170"},{"property":{"classname":"android.view.View","set":{"parameters":[{"type":"java.lang.Integer"}],"selector":"setVisibility"},"name":"hidden","get":{"result":{"type":"java.lang.Integer"},"parameters":[],"selector":"getVisibility"}},"path":[{"index":0,"prefix":"shortest","id":16908290},{"index":0,"view_class":"android.widget.LinearLayout"},{"index":0,"mp_id_name":"edit_email_address"}],"args":[[4,"java.lang.Integer"]],"change_type":"property","name":"c272"},{"property":{"classname":"android.view.View","set":{"parameters":[{"type":"java.lang.Float"}],"selector":"setAlpha"},"name":"alpha","get":{"result":{"type":"java.lang.Float"},"parameters":[],"selector":"getAlpha"}},"path":[{"index":0,"prefix":"shortest","id":16908290},{"index":0,"view_class":"android.widget.LinearLayout"},{"index":1,"view_class":"android.widget.Button"}],"args":[[0.32,"java.lang.Float"]],"change_type":"property","name":"c323"}],"id":8974,"experiment_id":4290}]}'
+
         f = open('response.txt', 'w')
         f.write(text_size_change_response)
         f.close()
         self._launch_app()
-        self.assertEquals(self.driver.find_element_by_id('edit_first_name').get_attribute('textSize'), '108.0')
+        self.assertEquals(self.driver.find_element_by_id('send_to_mixpanel').get_attribute('textSize'), '108.0')
 
 if __name__ == '__main__':
 	unittest.main()
