7c17757ffd67d52540dcca8bf992cfa99e33aa9a
Yinfei Ru <yinfei@mixpanel.com>
2015-05-07 16:46:25 -0700

    dry out code into _launch_app

--- acceptance/test_property.py
+++ acceptance/test_property.py
@@ -2,7 +2,11 @@ import unittest
 from selenium import webdriver
 
 class AndroidTest(unittest.TestCase):
-    def _launch_app(self):
+    def _launch_app(self, decide_message):
+        f = open('response.txt', 'w')
+        f.write(decide_message)
+        f.close()
+
         desired_capabilities = {'aut': 'com.mixpanel.example.hello:1.0'}
         self.driver = webdriver.Remote(
             desired_capabilities=desired_capabilities
@@ -14,12 +18,9 @@ class AndroidTest(unittest.TestCase):
         self.driver.quit()
 
     def test_change_text_size(self):
-        text_size_change_response = '{"notifications":[],"surveys":[],"variants":[{"tweaks":[],"actions":[{"property":{"classname":"android.widget.TextView","set":{"parameters":[{"type":"java.lang.Integer"},{"type":"java.lang.Float"}],"selector":"setTextSize"},"name":"fontSize","get":{"result":{"type":"java.lang.Float"},"parameters":[],"selector":"getTextSize"}},"path":[{"index":0,"prefix":"shortest","id":16908290},{"index":0,"view_class":"android.widget.RelativeLayout"},{"index":0,"mp_id_name":"edit_first_name"}],"args":[[2,"java.lang.Integer"],[36,"java.lang.Float"]],"change_type":"property","name":"c65"}],"id":8990,"experiment_id":4302}]}'
+        decide_message = '{"notifications":[],"surveys":[],"variants":[{"tweaks":[],"actions":[{"property":{"classname":"android.widget.TextView","set":{"parameters":[{"type":"java.lang.Integer"},{"type":"java.lang.Float"}],"selector":"setTextSize"},"name":"fontSize","get":{"result":{"type":"java.lang.Float"},"parameters":[],"selector":"getTextSize"}},"path":[{"index":0,"prefix":"shortest","id":16908290},{"index":0,"view_class":"android.widget.RelativeLayout"},{"index":0,"mp_id_name":"edit_first_name"}],"args":[[2,"java.lang.Integer"],[36,"java.lang.Float"]],"change_type":"property","name":"c65"}],"id":8990,"experiment_id":4302}]}'
+        self._launch_app(decide_message)
 
-        f = open('response.txt', 'w')
-        f.write(text_size_change_response)
-        f.close()
-        self._launch_app()
         self.assertEquals(self.driver.find_element_by_id('edit_first_name').get_attribute('textSize'), '108.0')
 
 if __name__ == '__main__':
