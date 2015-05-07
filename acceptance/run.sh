e5fbafb4117804d552f251b942baa32e7692a684
Yinfei Ru <yinfei@mixpanel.com>
2015-05-07 12:00:04 -0700

    more layout tests

--- acceptance/run.sh
+++ acceptance/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEST_APP_DIR=test-application
-TESTS=(property_test.py layout_test.py)
+TESTS=(test_property.py test_layout.py)
 HTTP_SERVER_PID=NULL
 SELEDROID_PID=NULL
 ANDROID_SDK_DIR=~/Library/Android/sdk/
--- acceptance/test_property.py
+++ acceptance/test_property.py
@@ -22,16 +22,5 @@ class AndroidTest(unittest.TestCase):
         self._launch_app()
         self.assertEquals(self.driver.find_element_by_id('edit_first_name').get_attribute('textSize'), '108.0')
 
-    def test_layout_change_basic(self):
-        layout_change_response = '{"notifications":[],"surveys":[],"variants":[{"tweaks":[],"actions":[{"args":[{"view_id_name":"send_to_mixpanel","verb":3,"anchor_id_name":"0"}],"name":"c155","path":[{"prefix":"shortest","index":0,"id":16908290},{"view_class":"android.widget.RelativeLayout","index":0}],"change_type":"layout"},{"args":[{"view_id_name":"send_to_mixpanel","verb":11,"anchor_id_name":"-1"}],"name":"c155","path":[{"prefix":"shortest","index":0,"id":16908290},{"view_class":"android.widget.RelativeLayout","index":0}],"change_type":"layout"},{"args":[{"view_id_name":"send_to_mixpanel","verb":3,"anchor_id_name":"edit_first_name"}],"name":"c155","path":[{"prefix":"shortest","index":0,"id":16908290},{"view_class":"android.widget.RelativeLayout","index":0}],"change_type":"layout"}],"id":8990,"experiment_id":4302}]}'
-
-        f = open('response.txt', 'w')
-        f.write(layout_change_response)
-        f.close()
-        self._launch_app()
-        self.assertTrue(self.driver.find_element_by_id('send_to_mixpanel').location['y'] < self.driver.find_element_by_id('edit_email_address').location['y'])
-        self.assertTrue(self.driver.find_element_by_id('send_to_mixpanel').location['x'] > self.driver.find_element_by_id('edit_email_address').location['x'])
-        self.assertEquals(self.driver.find_element_by_id('send_revenue').location['y'], self.driver.find_element_by_id('edit_email_address').location['y'])
-
 if __name__ == '__main__':
 	unittest.main()
