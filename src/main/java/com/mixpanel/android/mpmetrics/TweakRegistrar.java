710ae8fa99df3a07c645186ec22b984410813001
Joe <joe@mixpanel.com>
2015-06-05 14:44:28 -0700

    Dead code

--- src/main/java/com/mixpanel/android/mpmetrics/TweakRegistrar.java
+++
@@ -1,9 +0,0 @@
-package com.mixpanel.android.mpmetrics;
-
-/**
- * This interface is for
- */
-public interface TweakRegistrar {
-    void declareTweaks(Tweaks t);
-    void registerObjectForTweaks(Tweaks t, Object registrant);
-}
