497e8ffc76a6bd0a4b9419ff968e18582989b659
Yinfei Ru <yinfei@mixpanel.com>
2015-08-24 17:04:14 -0700

    bump up the version to 4.6.4

--- src/main/java/com/mixpanel/android/mpmetrics/MPConfig.java
+++ src/main/java/com/mixpanel/android/mpmetrics/MPConfig.java
@@ -88,7 +88,7 @@ public class MPConfig {
     // Unfortunately, as long as we support building from source in Eclipse,
     // we can't rely on BuildConfig.MIXPANEL_VERSION existing, so this must
     // be hard-coded both in our gradle files and here in code.
-    public static final String VERSION = "4.6.4-SNAPSHOT";
+    public static final String VERSION = "4.6.4";
 
     public static boolean DEBUG = false;
 
