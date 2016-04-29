dcee85de9d8c1e41a238ec04a5df114f40842bad
Yarden Eitan <yarneo@gmail.com>
2016-04-29 12:43:00 -0700

    indentation

--- src/main/java/com/mixpanel/android/viewcrawler/GestureTracker.java
+++ src/main/java/com/mixpanel/android/viewcrawler/GestureTracker.java
@@ -14,90 +14,90 @@ import com.mixpanel.android.mpmetrics.MixpanelAPI;
  **/
 public class GestureTracker {
 
-	public GestureTracker(MixpanelAPI mMixpanel, Activity parent) {
-		trackGestures(mMixpanel, parent);
-	}
+    public GestureTracker(MixpanelAPI mMixpanel, Activity parent) {
+        trackGestures(mMixpanel, parent);
+    }
 
-	private void trackGestures(final MixpanelAPI mMixpanel, final Activity parent) {
-		parent.getWindow().getDecorView().setOnTouchListener(getGestureTrackerTouchListener(mMixpanel));
-	}
+    private void trackGestures(final MixpanelAPI mMixpanel, final Activity parent) {
+        parent.getWindow().getDecorView().setOnTouchListener(getGestureTrackerTouchListener(mMixpanel));
+    }
 
-	private View.OnTouchListener getGestureTrackerTouchListener(final MixpanelAPI mMixpanel) {
-		return new View.OnTouchListener() {
+    private View.OnTouchListener getGestureTrackerTouchListener(final MixpanelAPI mMixpanel) {
+        return new View.OnTouchListener() {
 
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				if (event.getPointerCount() > 2) {
-					resetGesture();
-					return false;
-				}
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getPointerCount() > 2) {
+                    resetGesture();
+                    return false;
+                }
 
-				switch (event.getActionMasked()) {
-					case MotionEvent.ACTION_DOWN:
-						mFirstToSecondFingerDifference = System.currentTimeMillis();
-						break;
-					case MotionEvent.ACTION_POINTER_DOWN:
-						if ((System.currentTimeMillis() - mFirstToSecondFingerDifference) < TIME_BETWEEN_FINGERS_THRESHOLD) {
-							if (System.currentTimeMillis() - mTimePassedBetweenTaps > TIME_BETWEEN_TAPS_THRESHOLD) {
-								resetGesture();
-							}
-							mSecondFingerTimeDown = System.currentTimeMillis();
-							mDidTapDownBothFingers = true;
-						} else {
-							resetGesture();
-						}
-						break;
-					case MotionEvent.ACTION_POINTER_UP:
-						if (mDidTapDownBothFingers) {
-							mFirstToSecondFingerDifference = System.currentTimeMillis();
-						} else {
-							resetGesture();
-						}
-						break;
-					case MotionEvent.ACTION_UP:
-						if ((System.currentTimeMillis() - mFirstToSecondFingerDifference) < TIME_BETWEEN_FINGERS_THRESHOLD) {
-							if ((System.currentTimeMillis() - mSecondFingerTimeDown) >= TIME_FOR_LONG_TAP) {
-								// Long Tap
-								if (mGestureSteps == 3) {
-									mMixpanel.track("$ab_gesture1");
-									resetGesture();
-								}
-								mGestureSteps = 0;
-							} else {
-								// Short Tap
-								mTimePassedBetweenTaps = System.currentTimeMillis();
-								if (mGestureSteps < 4) {
-									mGestureSteps += 1;
-								} else if (mGestureSteps == 4) {
-									mMixpanel.track("$ab_gesture2");
-									resetGesture();
-								} else {
-									resetGesture();
-								}
-							}
-						}
-						break;
-				}
-				return false;
-			}
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        mFirstToSecondFingerDifference = System.currentTimeMillis();
+                        break;
+                    case MotionEvent.ACTION_POINTER_DOWN:
+                        if ((System.currentTimeMillis() - mFirstToSecondFingerDifference) < TIME_BETWEEN_FINGERS_THRESHOLD) {
+                            if (System.currentTimeMillis() - mTimePassedBetweenTaps > TIME_BETWEEN_TAPS_THRESHOLD) {
+                                resetGesture();
+                            }
+                            mSecondFingerTimeDown = System.currentTimeMillis();
+                            mDidTapDownBothFingers = true;
+                        } else {
+                            resetGesture();
+                        }
+                        break;
+                    case MotionEvent.ACTION_POINTER_UP:
+                        if (mDidTapDownBothFingers) {
+                            mFirstToSecondFingerDifference = System.currentTimeMillis();
+                        } else {
+                            resetGesture();
+                        }
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if ((System.currentTimeMillis() - mFirstToSecondFingerDifference) < TIME_BETWEEN_FINGERS_THRESHOLD) {
+                            if ((System.currentTimeMillis() - mSecondFingerTimeDown) >= TIME_FOR_LONG_TAP) {
+                                // Long Tap
+                                if (mGestureSteps == 3) {
+                                    mMixpanel.track("$ab_gesture1");
+                                    resetGesture();
+                                }
+                                mGestureSteps = 0;
+                            } else {
+                                // Short Tap
+                                mTimePassedBetweenTaps = System.currentTimeMillis();
+                                if (mGestureSteps < 4) {
+                                    mGestureSteps += 1;
+                                } else if (mGestureSteps == 4) {
+                                    mMixpanel.track("$ab_gesture2");
+                                    resetGesture();
+                                } else {
+                                    resetGesture();
+                                }
+                            }
+                        }
+                        break;
+                }
+                return false;
+            }
 
-			private void resetGesture() {
-				mFirstToSecondFingerDifference = -1;
-				mSecondFingerTimeDown = -1;
-				mGestureSteps = 0;
-				mTimePassedBetweenTaps = -1;
-				mDidTapDownBothFingers = false;
-			}
+            private void resetGesture() {
+                mFirstToSecondFingerDifference = -1;
+                mSecondFingerTimeDown = -1;
+                mGestureSteps = 0;
+                mTimePassedBetweenTaps = -1;
+                mDidTapDownBothFingers = false;
+            }
 
-			private long mSecondFingerTimeDown = -1;
-			private long mFirstToSecondFingerDifference = -1;
-			private int mGestureSteps = 0;
-			private long mTimePassedBetweenTaps = -1;
-			private boolean mDidTapDownBothFingers = false;
-			private final int TIME_BETWEEN_FINGERS_THRESHOLD = 100;
-			private final int TIME_BETWEEN_TAPS_THRESHOLD = 1000;
-			private final int TIME_FOR_LONG_TAP = 2500;
-		};
-	}
+            private long mSecondFingerTimeDown = -1;
+            private long mFirstToSecondFingerDifference = -1;
+            private int mGestureSteps = 0;
+            private long mTimePassedBetweenTaps = -1;
+            private boolean mDidTapDownBothFingers = false;
+            private final int TIME_BETWEEN_FINGERS_THRESHOLD = 100;
+            private final int TIME_BETWEEN_TAPS_THRESHOLD = 1000;
+            private final int TIME_FOR_LONG_TAP = 2500;
+        };
+    }
 
 }
