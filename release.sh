0ea48d3d030d5315e65cebe5a4537b7416c078ee
Sergio Alonso <sergio.alonso@mixpanel.com>
2017-02-15 20:10:36 -0800

    update release script

--- release.sh
+++ release.sh
@@ -6,12 +6,22 @@
 # If no version is given the next release version used will be the one that appears
 # on gradle.properties (VERSION_NAME).
 
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+NC='\033[0m'
+
 if [ ! -f gradle.properties ]; then
-    echo "gradle.properties was not found. Make sure you are running this script from its root folder." 
+    printf "${RED}gradle.properties was not found. Make sure you are running this script from its root folder${NC}\n" 
     exit
 fi
 if [ ! -f ~/.gradle/gradle.properties.bak ]; then
-    echo "~/.gradle/gradle.properties.bak was not found" 
+    printf "${RED}~/.gradle/gradle.properties.bak was not found${NC}\n" 
+    exit
+fi
+if [[ ! -z $(git status -s) ]]; then
+    printf "${RED}You have unstaged/untracked changes${NC}\n"
     exit
 fi
 
@@ -23,7 +33,7 @@ abort () {
 
 quit () {
     mv ~/.gradle/gradle.properties ~/.gradle/gradle.properties.bak
-    git checkout $currentBranch
+    git checkout $originalBranch
     exit
 }
 
@@ -34,22 +44,15 @@ cleanUp () {
     if [ -f README.md.bak ]; then
         rm README.md.bak  
     fi
+    if [ -f changes.txt ]; then
+        rm changes.txt 
+    fi
 }
 
 restoreFiles () {
     git checkout -- gradle.properties
     git checkout -- README.md
 }
-
-mv ~/.gradle/gradle.properties.bak ~/.gradle/gradle.properties
-
-currentBranch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-releaseBranch=master
-docBranch=gh-pages
-
-# checkout release branch
-git checkout $releaseBranch
-git pull origin $releaseBranch
 
 # find release version: if no args we grab gradle.properties without -SNAPSHOT
 if [ -z "$1" ]
@@ -58,37 +61,51 @@ if [ -z "$1" ]
 else
     releaseVersion=$1
 fi
+echo $releaseVersion | grep -q "^[0-9]\+.[0-9]\+.[0-9]$"
+if [ ! $? -eq 0 ] ;then
+    printf "${RED}Wrong version format (X.X.X) for: $releaseVersion\n"
+    printf "Check your gradle.properties file or the argument you passed.${NC}\n"
+    exit
+fi
+ 
+originalBranch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+releaseBranch=master
+docBranch=gh-pages
+
+mv ~/.gradle/gradle.properties.bak ~/.gradle/gradle.properties
+
+# checkout release branch
+printf "${YELLOW}Checking out $releaseBranch...${NC}\n"
+git checkout $releaseBranch
+git pull origin $releaseBranch
 
 # find next snapshot version by incrementing the release version
 nextSnapshotVersion=$(echo $releaseVersion | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')-SNAPSHOT
 
 # change version on gradle.properties - Make sure there are no spaces. Expected format: VERSION_NAME=.*
-sed -i.bak 's,^\(VERSION_NAME=\).*,\1'$releaseVersion',' gradle.properties
+sed -i.bak 's,^\(VERSION_NAME=\).*,\1'$releaseVersion',w changes.txt' gradle.properties
+if [ ! -s changes.txt ]; then
+    printf "\n${RED}Err... gradle.properties was not updated. The following command was used:\n"
+    printf "sed -i.bak 's,^\(VERSION_NAME=\).*,\1'$releaseVersion',' gradle.properties${NC}\n\n"
+    abort
+fi
+rm changes.txt
 
 # change date latest release
 newDate=$(date "+%B %d\, %Y") # Need the slash before the comma so next command does not fail
-sed -i.bak "s,^\(##### _\).*\(_ - \[v\).*\(](https://github.com/mixpanel/mixpanel-android/releases/tag/v\).*\()\),\1$newDate\2$releaseVersion\3$releaseVersion\4," README.md
-
-printf '\n\n'
-if [ ! -f README.md.bak ]; then
-    echo "Err... README.md was not updated. The following command was used:"
-    echo "sed -i.bak 's,^\(##### _\).*\(_ - \[v\).*\(](https://github.com/mixpanel/mixpanel-android/releases/tag/v\).*\()\),\1$newDate\2$releaseVersion\3$releaseVersion\4,' README.md"
+sed -i.bak "s,^\(##### _\).*\(_ - \[v\).*\(](https://github.com/mixpanel/mixpanel-android/releases/tag/v\).*\()\),\1$newDate\2$releaseVersion\3$releaseVersion\4,w changes.txt" README.md
+if [ ! -s changes.txt ]; then
+    printf "\n${RED}Err... README.md was not updated. The following command was used:\n"
+    printf "sed -i.bak 's,^\(##### _\).*\(_ - \[v\).*\(](https://github.com/mixpanel/mixpanel-android/releases/tag/v\).*\()\),\1$newDate\2$releaseVersion\3$releaseVersion\4,' README.md${NC}\n\n"
     abort
 fi
 
-if [ ! -f gradle.properties.bak ]; then
-    echo "Err... gradle.properties was not updated. The following command was used:"
-    echo "sed -i.bak 's,^\(VERSION_NAME=\).*,\1'$releaseVersion',' gradle.properties"
-    abort
-fi
-
-printf "New gradle.properties:\n"
-printf '%s\n' '-----------------------'
+printf "\n"
+printf "${GREEN}New gradle.properties:${NC}\n"
 head -n 1 gradle.properties
 printf '[....]\n\n\n'
 
-printf "New README.md:\n"
-printf '%s\n' '-----------------------'
+printf "${GREEN}New README.md:${NC}\n"
 head -n 9 README.md
 printf '[....]\n\n\n'
 
@@ -99,13 +116,13 @@ if ! [[ "$key" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
     abort
 fi
 
-# remove backup file
+# remove backup files
 cleanUp
 
 # upload library to maven
-printf '\n\nUploading archives....\n'
+printf "\n\n${YELLOW}Uploading archives...${NC}\n"
 if ! ./gradlew uploadArchives ; then
-    printf "Err.. Seems there was a problem runing ./gradlew uploadArchives"
+    printf "${RED}Err.. Seems there was a problem runing ./gradlew uploadArchives\n${NC}"
     abort
 fi
 
@@ -115,47 +132,48 @@ if ! [[ "$key" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
 fi
 
 # commit new version
+printf "\n\n${YELLOW}Pushing changes...${NC}\n"
 git commit -am "New release: $releaseVersion"
-
 # push changes
 git push origin $releaseBranch
 
 # create new tag
 newTag=v$releaseVersion
+printf "\n\n${YELLOW}Creating new tag $newTag...${NC}\n"
 git tag $newTag
 git push origin $newTag
 
 # update next snapshot version
-printf '\nUpdating next snapshot version...\n'
+printf "\n${YELLOW}Updating next snapshot version...${NC}\n"
 sed -i.bak 's,^\(VERSION_NAME=\).*,\1'$nextSnapshotVersion',' gradle.properties
-printf "\nNew gradle.properties:\n"
-printf '%s\n' '-----------------------'
+printf "${GREEN}New gradle.properties:${NC}\n"
 head -n 1 gradle.properties
 printf '[....]\n\n\n'
 
 read -r -p "Does this look right to you? [y/n]: " key
 if [[ "$key" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
-    printf '\n\n'
     git commit -am "Update master with next snasphot version $nextSnapshotVersion"
     git push origin master
 else
-    printf "\nReverting.... Make sure to update this manually.\n"
+    printf "${ORANGE}Make sure to update gradle.properties manually.${NC}\n"
     restoreFiles
 fi
 
+# remove backup files
 cleanUp
 
 # update documentation
-printf '\n\nUpdating documentation...\n\n'
+printf "\n\n${YELLOW}Updating documentation...${NC}\n\n"
 git checkout $docBranch
 git pull origin $docBranch
 cp -r build/docs/javadoc/* .
-git commit -am "Update documentation for $releaseVersion"
+git add .
+git commit -m "Update documentation for $releaseVersion"
 git push origin gh-pages
 
-printf '\nAll done!\n'
-printf 'Make sure you make a new release at https://github.com/mixpanel/mixpanel-android/releases/new\n'
-printf 'Also, do not forget to update our CHANGELOG (https://github.com/mixpanel/mixpanel-android/wiki/Changelog)\n'
-printf 'And finally, release the library from https://oss.sonatype.org/index.html\n\n'
+printf "\n${GREEN}All done! ¯\_(ツ)_/¯ \n"
+printf "Make sure you make a new release at https://github.com/mixpanel/mixpanel-android/releases/new\n"
+printf "Also, do not forget to update our CHANGELOG (https://github.com/mixpanel/mixpanel-android/wiki/Changelog)\n"
+printf "And finally, release the library from https://oss.sonatype.org/index.html\n\n${NC}"
 
 quit
