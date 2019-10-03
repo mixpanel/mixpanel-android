7d1a99678d9c8d33448593e4370fa1f483a87514
Austen Castberg <austen98@gmail.com>
2019-10-03 12:52:16 -0500

    configure webview settings

--- src/main/java/com/mixpanel/android/mpmetrics/MixpanelWebViewActivity.java
+++ src/main/java/com/mixpanel/android/mpmetrics/MixpanelWebViewActivity.java
@@ -2,6 +2,8 @@ package com.mixpanel.android.mpmetrics;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -16,7 +18,19 @@ public class MixpanelWebViewActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.com_mixpanel_android_activity_webview);
         WebView webView = findViewById(R.id.com_mixpanel_android_activity_webview_view);
+
         webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowFileAccess(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setDomStorageEnabled(true);
+
         webView.loadUrl(getIntent().getExtras().getString("uri"));
     }
 }
