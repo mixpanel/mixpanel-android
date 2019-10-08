8d92ecefe85efefd9eef49f26d87c6af11bf4be7
Austen Castberg <austen98@gmail.com>
2019-10-08 18:50:07 -0500

    fix url checking for webviews to prevent http attempts

--- src/main/java/com/mixpanel/android/mpmetrics/MixpanelNotificationRouteActivity.java
+++ src/main/java/com/mixpanel/android/mpmetrics/MixpanelNotificationRouteActivity.java
@@ -56,19 +56,19 @@ public class MixpanelNotificationRouteActivity extends Activity {
             case HOMESCREEN:
                 return defaultIntent;
             case URL_IN_BROWSER:
-                if (isValidURL(uri.toString())) {
+                if (isValidURL(uri.toString(), PushTapTarget.URL_IN_BROWSER)) {
                     return new Intent(Intent.ACTION_VIEW, Uri.parse(uri.toString()));
                 } else {
                     MPLog.d(LOGTAG, "Wanted to open url in browser but url is invalid: " + uri.toString() + ". Starting default intent");
                     return defaultIntent;
                 }
             case URL_IN_WEBVIEW:
-                if (isValidURL(uri.toString())) {
+                if (isValidURL(uri.toString(), PushTapTarget.URL_IN_WEBVIEW)) {
                     return new Intent(this.getApplicationContext(), MixpanelWebViewActivity.class).
                             putExtra("uri", uri.toString()).
                             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 } else {
-                    MPLog.d(LOGTAG, "Wanted to open url in webview but url is invalid: " + uri.toString() + ". Starting default intent");
+                    MPLog.d(LOGTAG, "Wanted to open url in webview but url is invalid or not secure (http): " + uri.toString() + ". Starting default intent");
                     return defaultIntent;
                 }
             case DEEP_LINK:
@@ -78,8 +78,12 @@ public class MixpanelNotificationRouteActivity extends Activity {
         }
     }
 
-    protected boolean isValidURL(CharSequence url) {
-        return null != url && url.toString().startsWith("http");
+    protected boolean isValidURL(CharSequence url, PushTapTarget target) {
+        if (target == PushTapTarget.URL_IN_WEBVIEW) {
+            return null != url && url.toString().startsWith("https");
+        } else {
+            return null != url && url.toString().startsWith("http");
+        }
     }
 
     protected void cancelNotification(Bundle extras) {
