ba5f0456ed13d5ef2c1fe226380d4bbf9f476c66
Austen Castberg <austen98@gmail.com>
2019-12-10 17:51:07 -0600

    remove unused import

--- src/main/java/com/mixpanel/android/mpmetrics/MixpanelPushNotification.java
+++ src/main/java/com/mixpanel/android/mpmetrics/MixpanelPushNotification.java
@@ -9,7 +9,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
