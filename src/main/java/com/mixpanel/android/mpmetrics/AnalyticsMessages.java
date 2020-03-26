ec009bfdc172e49b33ecbe904bfa51794b91ba19
Sergio Alonso <sergio.alonso@mixpanel.com>
2020-03-25 21:03:25 -0700

    Add  property to super properties

--- src/main/java/com/mixpanel/android/mpmetrics/AnalyticsMessages.java
+++ src/main/java/com/mixpanel/android/mpmetrics/AnalyticsMessages.java
@@ -708,6 +708,10 @@ import javax.net.ssl.SSLSocketFactory;
                 if (null != isWifi)
                     ret.put("$wifi", isWifi.booleanValue());
 
+                final String radio = mSystemInformation.getPhoneRadioType(mContext);
+                if (null != radio)
+                    ret.put("$radio", radio);
+
                 final Boolean isBluetoothEnabled = mSystemInformation.isBluetoothEnabled();
                 if (isBluetoothEnabled != null)
                     ret.put("$bluetooth_enabled", isBluetoothEnabled);
