cc8ce0cb08b03b0f0e5b8f1e5bcf022890cb462b
Zihe Jia <zihe.jia@mixpanel.com>
2024-09-06 10:59:22 -0700

    clean up the dependencies

--- mixpaneldemo/build.gradle.kts
+++ mixpaneldemo/build.gradle.kts
@@ -50,7 +50,6 @@ android {
 }
 
 dependencies {
-    implementation("com.mixpanel.android:mixpanel-android:7.5.2")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
@@ -61,6 +60,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(project(":"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
