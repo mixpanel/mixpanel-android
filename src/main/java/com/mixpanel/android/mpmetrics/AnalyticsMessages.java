bf1e5e0113a489a4f16decabb9929d1b2cd366df
Rahul R <rahul.raveendran@mixpanel.com>
2026-01-19 18:55:43 +0530

    Log error on URL host extraction failure

--- src/main/java/com/mixpanel/android/mpmetrics/AnalyticsMessages.java
+++ src/main/java/com/mixpanel/android/mpmetrics/AnalyticsMessages.java
@@ -206,6 +206,7 @@ import org.json.JSONObject;
         try {
             return new URL(urlString).getHost();
         } catch (Exception e) {
+            MPLog.e(LOGTAG, "Could not extract host from URL " + urlString, e);
             return DEFAULT_SERVER_HOST;
         }
     }
