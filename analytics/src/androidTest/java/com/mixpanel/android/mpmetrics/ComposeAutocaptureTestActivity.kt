1ed8777922bd2b28fbf90a1376b095856972538a
Rahul Raveendran V P <rahul.raveendran@mixpanel.com>
2026-07-02 18:15:53 +0530

    Revert "refactor(test): move autocapture test activities to autocapture package"
    This reverts commit 6bcb552d73862a2aad87e8349a4097f85e0ee0d5.

--- analytics/src/androidTest/java/com/mixpanel/android/mpmetrics/ComposeAutocaptureTestActivity.kt
+++ analytics/src/androidTest/java/com/mixpanel/android/mpmetrics/ComposeAutocaptureTestActivity.kt
@@ -1,4 +1,4 @@
-package com.mixpanel.android.autocapture
+package com.mixpanel.android.mpmetrics
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
--- analytics/src/androidTest/java/com/mixpanel/android/mpmetrics/XmlAutocaptureTestActivity.java
+++ analytics/src/androidTest/java/com/mixpanel/android/mpmetrics/XmlAutocaptureTestActivity.java
@@ -1,4 +1,4 @@
-package com.mixpanel.android.autocapture;
+package com.mixpanel.android.mpmetrics;
 
 import android.app.Activity;
 import android.os.Bundle;
