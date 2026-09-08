03dbda3a7a727d5fc4db2045925c68d1b401e681
Tyler Roach <tyler.roach@mixpanel.com>
2026-09-08 09:40:29 -0400

    fix(session-replay): make event publishing thread-safe

--- session-replay/src/main/java/com/mixpanel/android/sessionreplay/tracking/EventPublisher.kt
+++ session-replay/src/main/java/com/mixpanel/android/sessionreplay/tracking/EventPublisher.kt
@@ -3,6 +3,7 @@ package com.mixpanel.android.sessionreplay.tracking
 import com.mixpanel.android.sessionreplay.models.RawScreenshotEvent
 import com.mixpanel.android.sessionreplay.models.RawTouchEvent
 import com.mixpanel.android.sessionreplay.models.SessionEvent
+import java.util.concurrent.CopyOnWriteArrayList
 
 class EventPublisher {
     companion object {
@@ -11,7 +12,7 @@ class EventPublisher {
 
     private constructor() // Private constructor to enforce singleton pattern
 
-    private val subscribers = mutableListOf<EventListener>()
+    private val subscribers = CopyOnWriteArrayList<EventListener>()
 
     fun subscribe(subscriber: EventListener) {
         subscribers.add(subscriber)
